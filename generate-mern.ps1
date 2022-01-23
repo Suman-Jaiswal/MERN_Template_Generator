9f76565acc945e65861da749c69ab806835a6e8d
Suman <sumanj631@gmai.com>
2022-01-23 18:50:48 +0530

    .

---
+++ generate-mern.ps1
@@ -0,0 +1,195 @@
+echo ""
+Write-Host "Making Root directories (client and server)..." -ForegroundColor blue
+echo ""
+mkdir client
+mkdir server
+cd client
+echo ""
+Write-Host "Installing latest version of React..." -ForegroundColor blue
+echo ""
+npx create-react-app .
+cd ..
+cd server
+echo ""
+Write-Host "Creating Node server..." -ForegroundColor blue
+echo ""
+yarn init -y
+yarn add express dotenv mongoose cors express-async-handler esm
+
+
+
+
+# ----------------------------------------------------------------------------------------------------->
+New-Item index.js
+Set-Content index.js "require = require('esm')(module)
+module.exports = require('./server.js')"
+# -------------------------------------------------------------------------------------------------------->
+
+
+
+
+# ----------------------------------------------------------------------------------------------------->
+New-Item server.js
+Set-Content server.js "import connectDB from './config/db.js'
+import userRoutes from './routes/userRoute.js'
+import express from 'express'
+import dotenv  from 'dotenv'
+
+//connect database
+connectDB()
+
+//dotenv config
+dotenv.config()
+
+const app = express()
+
+//Creating API for user
+app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+
+//Express js listen method to run project on http://localhost:5000
+app.listen(PORT, console.log('App is running in ' + process.env.NODE_ENV + ' mode on port ' + PORT))"
+# -------------------------------------------------------------------------------------------------------->
+
+
+
+
+echo ""
+Write-Host "Creating basic backend..." -ForegroundColor blue
+echo ""
+mkdir controllers, models, routes, config
+
+
+
+
+# --------------------------------------------------------------------------------------------------------->
+New-Item config/db.js
+Set-Content config/db.js "const mongoose = require('mongoose')
+
+const DB_URI = process.env.MONGO_URI
+
+const connectDB = async () => {
+   mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true
+})
+.then(res => console.log('mongoDB connected...'))
+.catch(err => console.log(err))
+}
+
+export default connectDB"
+# -------------------------------------------------------------------------------------------------------->
+
+
+
+
+# -------------------------------------------------------------------------------------------------------->
+New-Item controllers/userController.js
+Set-Content controllers/userController.js "import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
+
+//getUsers function to get all users
+export const getUsers = asyncHandler(async(req, res) => {
+    const users = await User.find({})
+    res.json(users)
+})
+
+//getUserById function to retrieve user by id
+export const getUserById  = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id)
+
+    //if user id match param id send user else throw error
+    if(user){
+        res.json(user)
+    }else{
+        res.status(404).json({message: 'User not found'})
+        res.status(404)
+        throw new Error('User not found')
+    }
+})"
+# -------------------------------------------------------------------------------------------------------->
+
+
+
+
+# -------------------------------------------------------------------------------------------------------->
+New-Item models/userModel.js
+Set-Content models/userModel.js "import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+    },
+    secondName:{
+        type: String,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        defualt: false
+    },
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User"
+# -------------------------------------------------------------------------------------------------------->
+
+
+
+
+# -------------------------------------------------------------------------------------------------------->
+New-Item routes/userRoute.js
+Set-Content routes/userRoute.js "import { getUsers, getUserById } from '../controllers/userController.js';
+import express from 'express'
+const router = express.Router()
+
+// express router method to create route for getting all users
+router.route('/').get(getUsers)
+
+// express router method to create route for getting users by id
+router.route('/:id').get(getUserById)
+
+export default router"
+# -------------------------------------------------------------------------------------------------------->
+
+
+
+
+# -------------------------------------------------------------------------------------------------------->
+echo ""
+Write-Host "Adding environment..." -ForegroundColor blue
+echo ""
+
+New-Item .env
+Set-Content .env "NODE_ENV = development
+PORT = 5000"
+
+cd ..
+echo ""
+Write-Host "Basic MERN template generated successfully!"  -ForegroundColor green
+echo ""
+Write-Host "(Now you can add mongodb credentials in .env)"  -ForegroundColor green
+Write-Host "Happy Coding!"  -ForegroundColor green
+echo ""
+Write-Host "(This script is made by SKJ)"  -ForegroundColor green
+
+# -------------------------------------------------------------------------------------------------------->
